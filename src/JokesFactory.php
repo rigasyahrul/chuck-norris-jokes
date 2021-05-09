e1bebd45fa907024f45da0151438e07c40f65287
rigasyahrul <riga@nusantarabetastudio.com>
2021-05-09 15:06:34 +0700

    Initial Commit

---
+++ src/JokesFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Rigasyahrul\ChuckNorrisJokes;
+
+class JokesFactory
+{
+    protected $jokes = [
+        'Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.',
+        'Chuck Norris breathes air … five times a day.',
+        'Time waits for no man. Unless that man is Chuck Norris.',
+        'Chuck Norris drinks napalm to fight his heartburn.',
+        ''
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+}
---
+++ tests/JokesFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Rigasyahrul\ChuckNorrisJokes\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Rigasyahrul\ChuckNorrisJokes\JokesFactory;
+
+class JokesFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokesFactory([
+            'This is a joke',
+        ]);
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'Chuck Norris doesn’t read books. He stares them down until he gets the information he wants.',
+            'Chuck Norris breathes air … five times a day.',
+            'Time waits for no man. Unless that man is Chuck Norris.',
+            'Chuck Norris drinks napalm to fight his heartburn.'
+        ];
+
+        $jokes = new JokesFactory();
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
+}
