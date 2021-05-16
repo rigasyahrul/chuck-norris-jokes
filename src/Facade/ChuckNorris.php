a49e0fbd070dcde5f0329c551fe2c2bb7b80ff07
rigasyahrul <riga@nusantarabetastudio.com>
2021-05-16 21:37:03 +0700

    docs(facade): adding reference to JokesFactory
    Took 4 minutes
    
    
    Took 11 seconds

--- src/Facade/ChuckNorris.php
+++ src/Facade/ChuckNorris.php
@@ -4,6 +4,12 @@ namespace Rigasyahrul\ChuckNorrisJokes\Facade;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class ChuckNorris
+ * @package Rigasyahrul\ChuckNorrisJokes\Facade
+ *
+ * @see \Rigasyahrul\ChuckNorrisJokes\JokesFactory
+ */
 class ChuckNorris extends Facade
 {
     protected static function getFacadeAccessor()
